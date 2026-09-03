62329dafcd2058732198d248db2b4aa739b1908b
Pablo Reyes <p.reyeshdez@gmail.com>
2026-09-02 21:45:44 -0400

    Actualiza frontend con URLs de produccion

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 const CONFIGURATOR_API_URL =
-  "https://beanies-configurator-api-git-deploy-vercel-preyeshdez.vercel.app";
+  "https://beanies-configurator-api.vercel.app";
 
 const ECOMMERCE_API_URL =
-  "https://beanies-ecommerce-api-git-deploy-vercel-preyeshdez.vercel.app";
+  "https://beanies-ecommerce-api.vercel.app";
